b82255c1fb384b1ddfa5615210a3d33175721ff2
Kobra777 <47458408+Kobra777@users.noreply.github.com>
2024-11-13 17:07:53 +0100

    ConsegnaS2L3

---
+++ ConsegnaS2L3.py
@@ -0,0 +1,11 @@
+print("------------------------------------")
+print("---      GENERATORE DI BAND      ---")
+print("------------------------------------")
+
+citta = input("Inserisci il nome della tua città: ")
+animale = input("Inserisci il nome del tuo animale: ")
+
+nome_band = citta + " " + animale
+
+
+print("\n      ---", nome_band ,"---            ")
